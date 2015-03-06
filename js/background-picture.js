0ecef64506f55bc0bd1445eb900ccb5c7c5858e9
reggie duran <reggieduran15@gmail.com>
2015-03-06 15:19:35 +0800

    done with the responsive design for mobile

--- js/background-picture.js
+++ js/background-picture.js
@@ -4,14 +4,9 @@
 
 jQuery(document).ready(function($) {
   $('.hentry').height($(window).height());
-  $(window).resize(function() {
-    $('.hentry').height($(window).height());
-  });
-});
-
-jQuery(document).ready(function($) {
   $('.hentry').width($(window).width());
   $(window).resize(function() {
+    $('.hentry').height($(window).height());
     $('.hentry').width($(window).width());
   });
 });
---
+++ js/min/background-picture-min.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function($){$(".hentry").height($(window).height()),$(".hentry").width($(window).width()),$(window).resize(function(){$(".hentry").height($(window).height()),$(".hentry").width($(window).width())})});
