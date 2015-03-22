123994010ee08bee127041bb94ace84e27272b1c
Benjamin Intal <bfintal@gmail.com>
2015-03-22 21:26:40 +0800

    Error 404 page
    Closes #52

--- 404.php
+++ 404.php
@@ -13,36 +13,19 @@ get_header(); ?>
 			<section class="error-404 not-found">
 			
 				<div class="page-content">
+					
+					<span class="genericon genericon-close"></span>
+					
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'regala' ); ?></p>
 
 					<?php get_search_form(); ?>
+					
+					<div class='widget-area'>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+						<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-					<?php if ( regala_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php _e( 'Most Used Categories', 'regala' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'regala' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+						<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+					</div>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
